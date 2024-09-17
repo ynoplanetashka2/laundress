164e497c65c5a8dc58d1aa663b625558a815c359
alex <mrynoplanetashka@gmail.com>
2024-09-17 23:13:21 +0300

    add omit for booking ids

--- packages/frontend/src/api/getBookings.ts
+++ packages/frontend/src/api/getBookings.ts
@@ -3,13 +3,14 @@
 import type { Filter, WithId } from 'mongodb';
 import { executeMongo } from './executeMongo';
 import type { Booking } from '@/schemas/Booking';
+import { omit } from 'lodash';
 
 export async function getBookings(filter?: Filter<WithId<Booking>>) {
   return await executeMongo(async (db) => {
     const collection = db.collection<Booking>('booking');
     const booked = await (
-      filter ? collection.find<Booking>(filter) : collection.find()
+      filter ? collection.find(filter) : collection.find()
     ).toArray();
-    return booked;
+    return booked.map((booking) => omit(booking, '_id'));
   });
 }
--- packages/frontend/src/api/getWashingMachines.ts
+++ packages/frontend/src/api/getWashingMachines.ts
@@ -4,7 +4,9 @@ import type { Filter, WithId } from 'mongodb';
 import { executeMongo } from './executeMongo';
 import type { WashingMachine } from '@/schemas/WashingMachine';
 
-export async function getWashingMachines(filter?: Filter<WithId<WashingMachine>>) {
+export async function getWashingMachines(
+  filter?: Filter<WithId<WashingMachine>>,
+) {
   return await executeMongo(async (db) => {
     const collection = db.collection<WashingMachine>('washingMachine');
     const washingMachines = await (
