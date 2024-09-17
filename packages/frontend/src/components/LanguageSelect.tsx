233533b16806c2ba63e232bf6a5c480e696f616e
alex <mrynoplanetashka@gmail.com>
2024-09-17 22:46:37 +0300

    update LanguageSelect with mui

--- packages/frontend/src/components/LanguageSelect.tsx
+++ packages/frontend/src/components/LanguageSelect.tsx
@@ -1,15 +1,24 @@
 'use client';
 import { SUPPORTED_LOCALES, usePathname, useRouter } from '@/i18n/routing';
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  type SelectChangeEvent,
+} from '@mui/material';
 import { useLocale } from 'next-intl';
-import { useTransition, type ChangeEvent } from 'react';
+import { useId, useTransition } from 'react';
 
 export default function LanguageSelect() {
   const locale = useLocale();
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
   const pathname = usePathname();
+  const id = useId();
+  const languageSelectId = `language-select-${id}`;
 
-  function handleChange(event: ChangeEvent<HTMLSelectElement>) {
+  function handleChange(event: SelectChangeEvent<string>) {
     const nextLocale = event.target.value as (typeof SUPPORTED_LOCALES)[number];
     startTransition(() => {
       router.push(pathname, {
@@ -18,12 +27,22 @@ export default function LanguageSelect() {
     });
   }
   return (
-    <select defaultValue={locale} onChange={handleChange} disabled={isPending}>
-      {SUPPORTED_LOCALES.map((loc) => (
-        <option key={loc} value={loc}>
-          {loc}
-        </option>
-      ))}
-    </select>
+    <FormControl fullWidth>
+      <InputLabel htmlFor={languageSelectId}> language </InputLabel>
+      <Select<string>
+        id={languageSelectId}
+        value={locale}
+        variant="filled"
+        label="language"
+        onChange={handleChange}
+        disabled={isPending}
+      >
+        {SUPPORTED_LOCALES.map((loc) => (
+          <MenuItem key={loc} value={loc}>
+            {loc}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
   );
 }
