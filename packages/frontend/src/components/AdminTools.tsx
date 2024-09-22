3c544691276ee4343f398c3bae05694d714d6255
alex <mrynoplanetashka@gmail.com>
2024-09-22 20:10:03 +0300

    add AdminTools component

---
+++ packages/frontend/src/components/AdminTools.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { Box } from "@mui/material";
+import TagsEditList from "./TagsEditList";
+
+type Props = {
+  adminAccountsEmails: string[];
+  userAccountsEmails: string[];
+  washingMachinesLabels: string[];
+  onAdminAccountsEmailsUpdate: (emails: string[]) => void;
+  onUserAccountsEmailsUpdate: (emails: string[]) => void;
+  onWashingMachinesLabelsUdate: (washingMachinesLabels: string[]) => void;
+};
+
+export default function AdminTools({ adminAccountsEmails, onAdminAccountsEmailsUpdate, onUserAccountsEmailsUpdate, onWashingMachinesLabelsUdate, userAccountsEmails, washingMachinesLabels }: Props) {
+  return (
+    <Box className='flex flex-col gap-4'>
+      <TagsEditList label='аккаунты администраторов' tags={adminAccountsEmails} onChange={onAdminAccountsEmailsUpdate}/>
+      <TagsEditList label='аккаунты обычных пользователей' tags={userAccountsEmails} onChange={onUserAccountsEmailsUpdate}/>
+      <TagsEditList label='названия стиральных машинок' tags={washingMachinesLabels} onChange={onWashingMachinesLabelsUdate}/>
+    </Box>
+  )
+}
--- packages/frontend/src/components/TagsEditList.tsx
+++ packages/frontend/src/components/TagsEditList.tsx
@@ -1,27 +1,36 @@
 'use client';
 
-import type { ChangeEvent, CSSProperties } from "react";
+import { Box, Button, FormControl, FormHelperText, FormLabel, TextareaAutosize } from "@mui/material";
+import { useId, useRef, type ChangeEvent, type CSSProperties, type FormEvent } from "react";
 
 type Props = {
   tags: string[];
+  label: string;
   onChange?: (newTags: string[]) => void;
   onSave?: (tags: string[]) => void;
-  style?: CSSProperties;
 }
 
-export default function TagsEditList({ tags, onChange, style, onSave }: Props) {
+export default function TagsEditList({ tags, onSave, label }: Props) {
   const concatenatedTags = tags.join('\n');
-  function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
-    const newConcatenatedTags: string = event.target.value;
+  function handleSubmit(event: FormEvent<HTMLDivElement>) {
+    event.preventDefault();
+    const newConcatenatedTags: string = textareaRef.current!.value;
     const newSplittedTags = newConcatenatedTags.split('\n');
-    if (onChange) {
-      onChange(newSplittedTags);
+    if (onSave) {
+      onSave(newSplittedTags);
     }
   }
+  const id = useId();
+  const textareaId = `textarea-${id}`;
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   return (
-    <div>
-      <textarea value={concatenatedTags} onChange={handleChange} style={style}/> <br />
-      <button onClick={() => onSave && onSave(tags)}>save</button>
-    </div>
+    <FormControl onSubmit={handleSubmit}>
+      <Box>
+        <FormLabel htmlFor={textareaId}>{ label }</FormLabel>
+        <TextareaAutosize ref={textareaRef} id={textareaId} defaultValue={concatenatedTags} className='w-2/3 block resize mb-3' placeholder={label} />
+        <Button type='submit' color='primary' variant='contained' onClick={() => onSave && onSave(tags)}>save</Button>
+        <FormHelperText>{ label }</FormHelperText>
+      </Box>
+    </FormControl>
   )
 }
