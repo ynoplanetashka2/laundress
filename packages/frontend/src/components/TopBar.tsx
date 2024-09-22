788df9a1d3f6267a6dfd6f8f3920a19e561f704e
alex <mrynoplanetashka@gmail.com>
2024-09-22 18:55:53 +0300

    add TopBar component

---
+++ packages/frontend/src/components/TopBar.tsx
@@ -0,0 +1,26 @@
+import { AppBar, Toolbar, Box, Avatar } from '@mui/material';
+import LanguageSelect from './LanguageSelect';
+import { isNil } from 'lodash';
+
+type Props = {
+  avatarSrc?: string | undefined;
+  avatarAlt?: string | undefined;
+}
+
+export default function TopBar({ avatarSrc, avatarAlt }: Props) {
+  return (
+    <AppBar position="sticky">
+      <Toolbar className='max-w-[800px] mx-auto w-[80vw] sm:w-[100vw]'>
+        <Box className='max-w-30'>
+          <LanguageSelect />
+        </Box>
+        {
+          avatarAlt === undefined || avatarSrc === undefined 
+          ? null
+          : 
+        <Avatar alt={avatarAlt} src={avatarSrc} />
+        }
+      </Toolbar>
+    </AppBar>
+  );
+}
